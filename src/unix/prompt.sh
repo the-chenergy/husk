73577a98c52984cbe0f6749348a8e5239ef5276f
Qianlang Chen <qianlangchen@gmail.com>
2022-05-16 00:38:07 -0600

    prompt.sh: Fixed formatting on Bash

--- src/unix/prompt.sh
+++ src/unix/prompt.sh
@@ -9,8 +9,7 @@ source "${BASE_DIR}/../../lib/posh-git-sh/git-prompt.sh"
 # Set the prompt format to `user@host:short_working_dir [git_status] $ `
 case "${SHELL_NAME}" in
 "bash")
-	PROMPT_COMMAND="__posh_git_ps1 "
-	"'\u@\h:\[\e[1m\]\W\[\e[0m\]' ' \[\e[1m\]\$\[\e[0m\] ' "
+	PROMPT_COMMAND="__posh_git_ps1 '\u@\h:\[\e[1m\]\W\[\e[0m\]' ' \[\e[1m\]\$\[\e[0m\] ' "
 	;;
 "zsh") PS1="%n@%m:%B%1d%b\$(__posh_git_echo) %B\$%b " ;;
 esac
