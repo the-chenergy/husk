501a3585a6ae3bed06b72853bc24a8d3cf59c479
Qianlang Chen <qianlangchen@fb.com>
2022-05-24 13:08:00 -0700

    aliases.sh: Added command for checking active shell name

--- src/unix/aliases.sh
+++ src/unix/aliases.sh
@@ -16,3 +16,6 @@ function c() {
 function ruler() {
   /usr/bin/python3 -c "print(*(f'....:....{i % 10}' for i in range(1, ${1:-12} + 1)), sep='')"
 }
+
+# Prints the name of the running shell.
+alias what-shell="echo ${SHELL_NAME}"
