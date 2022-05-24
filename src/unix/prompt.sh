a6bacac3a25d44a5fbf16b9ebecec428a06181ab
Qianlang Chen <qianlangchen@fb.com>
2022-05-24 13:08:40 -0700

    prompt.sh: Simplified prompt style
    - Hid username and hostname
    - Made git status black

--- src/unix/prompt.sh
+++ src/unix/prompt.sh
@@ -10,7 +10,7 @@ source "${BASE_DIR}/../../lib/posh-git-sh/git-prompt.sh"
 # - Replaces bright colors with gray
 # - Hides stats with value zero
 function __git_status() {
-	local s=$(echo $(__posh_git_echo) | sed "s@\[0;9[0-9]m@\[0;37m@g" | sed "s@ [+~-]0@@g")
+	local s=$(echo $(__posh_git_echo) | sed "s@\[0;9[0-9]m@\[0m@g" | sed "s@ [+~-]0@@g")
 	if test -n "${s}"; then
 		echo " ${s}"
 	fi
@@ -20,9 +20,9 @@ function __git_status() {
 case "${SHELL_NAME}" in
 "bash")
 	function __set_ps1() {
-		PS1="\u@\h:\[\e[1m\]\W\[\e[0m\]$(__git_status) \[\e[1m\]\$\[\e[0m\] "
+		PS1="\[\e[1m\]\W\[\e[0m\]$(__git_status) \[\e[1m\]\$\[\e[0m\] "
 	}
 	PROMPT_COMMAND=__set_ps1
 	;;
-"zsh") PS1="%n@%m:%B%1~%b\$(__git_status) %B\$%b " ;;
+"zsh") PS1="%B%1~%b\$(__git_status) %B\$%b " ;;
 esac
