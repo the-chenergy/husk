872a43786f1dee2f741d33f79761f449917edab8
Qianlang Chen <the-chenergy@palm-tree.domain.com>
2022-09-15 11:25:10 -0700

    init.sh: Fix git pwd alias format
    The --show-prefix flag was not passed properly on Zsh.

--- src/unix/init.sh
+++ src/unix/init.sh
@@ -8,6 +8,6 @@ git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.ck checkout
 git config --global alias.co checkout
-git config --global alias.pwd rev-parse --show-prefix
+git config --global alias.pwd "rev-parse --show-prefix"
 git config --global alias.sm submodule
 git config --global alias.st status
